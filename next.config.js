d3f161b11186b2906ad0a77bad8580694466bf65
Touger Thao <tougerthao@gmail.com>
2022-04-10 10:10:47 -0500

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: ["https://jherr-pokemon.s3.us-west-1.amazonaws.com/"],
+  },
+};
