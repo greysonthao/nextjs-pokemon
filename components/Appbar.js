f1ea6cb9b745ab4d8b11b479815311e5f2dba1f9
Touger Thao <tougerthao@gmail.com>
2022-04-11 10:21:30 -0500

    changed navbar to red and fixed spacing issue

--- components/Appbar.js
+++ components/Appbar.js
@@ -1,32 +1,95 @@
 import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 import Link from "next/link";
-/* import { Link } from "@mui/material"; */
-export default function DenseAppBar() {
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import CatchingPokemonIcon from "@mui/icons-material/CatchingPokemon";
+import SearchIcon from "@mui/icons-material/Search";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "80%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
+
+export default function SearchAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
+      <AppBar position="static" sx={{ backgroundColor: "#FF0000" }}>
+        <Toolbar
+          sx={{
+            justifyContent: "space-between",
+          }}
+        >
           <Link href="/">
             <a>
-              <Typography variant="h6" color="inherit" component="div">
-                Pokemon
-              </Typography>
+              <Box
+                component="div"
+                marginRight={2}
+                noWrap
+                sx={{ flexGrow: 1, display: "flex", alignItems: "center" }}
+              >
+                <CatchingPokemonIcon />
+                <Typography variant="h6" marginLeft={1}>
+                  Pokédex
+                </Typography>
+              </Box>
             </a>
           </Link>
+
+          <Search
+            sx={{
+              display: { sm: "block" },
+              backgroundColor: "#CC0000",
+            }}
+          >
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
         </Toolbar>
       </AppBar>
     </Box>
